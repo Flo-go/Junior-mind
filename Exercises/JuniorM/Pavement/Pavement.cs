aa67323b19552e3c76943a4168ab8c871423eb7f
Flo-go <florentina.nicoara@yahoo.com>
2016-06-21 23:10:58 +0300

    Square Building Stones Solution, if we can cut the stones

---
+++ Exercises/JuniorM/Pavement/Pavement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pavement
+{
+    [TestClass]
+    public class Pavement
+    {
+        [TestMethod]
+
+        public void SquarePavement()
+        {
+            decimal pavementStones = CalculatePavementforSquare(6, 6, 4);
+            Assert.AreEqual(3, pavementStones);
+        }
+
+        decimal CalculatePavementforSquare(decimal squareLength, decimal squareWidth, decimal buildingStoneSize)
+        {
+            decimal stonesNo = squareLength * squareWidth / (buildingStoneSize * buildingStoneSize);
+            return Math.Ceiling(stonesNo);            
+        }
+
+    }
+}
