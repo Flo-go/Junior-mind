f6355762bfcc3ba135b36e3fceb61217cc5ede3a
Flo-go <florentina.nicoara@yahoo.com>
2016-06-21 17:46:32 +0300

    Mushrooms and Training Solutions

---
+++ Exercises/JuniorM/Mushrooms/Mushrooms.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms
+{
+    [TestClass]
+    public class Mushrooms
+    {
+        [TestMethod]
+
+        public void MushroomsSolution()
+        {
+            int RedMushrooms = CalculateRedMushrooms(18,5);
+            Assert.AreEqual(15, RedMushrooms);
+        }
+
+        int CalculateRedMushrooms(int TotalMushrooms, int RateRedvsWhiteMushrooms)
+        {
+            return TotalMushrooms*RateRedvsWhiteMushrooms/(RateRedvsWhiteMushrooms+1);
+        }
+
+    }
+}
---
+++ Exercises/JuniorM/Training/Training.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training
+
+{
+    [TestClass]
+    public class Training
+    {
+        [TestMethod]
+
+        public void TrainingSolution()
+        {
+            int Rehearsals = CalculateRehearsals(4);
+            Assert.AreEqual(16, Rehearsals);
+        }
+
+        int CalculateRehearsals(int NumberOfRounds)
+        {
+            return (NumberOfRounds-1)*NumberOfRounds+NumberOfRounds;
+        }
+
+    }
+}
