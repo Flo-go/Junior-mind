a162698fd0ff84be8812a4972064f828a1b729f4
Flo-go <florentina.nicoara@yahoo.com>
2016-06-23 00:48:17 +0300

    Wood Flooring Solution

---
+++ Exercises/JuniorM/WoodFlooring/WoodFlooring.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WoodFlooring
+{
+    [TestClass]
+    public class WoodFlooring
+    {
+        [TestMethod]
+
+        public void WoodFlooringSolution()
+        {
+            double woodFlooringQuantity = CalculateRequiredWoodFlooring(6, 4, 1.5, 0.5);
+            Assert.AreEqual(37, woodFlooringQuantity);
+        }
+
+        double CalculateRequiredWoodFlooring(double roomLength, double roomWidth, double floorboardLength, double floorboardWidth)
+        {
+            double quantity = roomLength * roomWidth / (floorboardLength * floorboardWidth);
+            return Math.Ceiling(quantity+quantity*15/100);
+        }
+
+    }
+}
