03107cf0115921588e8e7d12ce2e59a7d2f85e8d
Flo-go <florentina.nicoara@yahoo.com>
2016-06-23 20:32:16 +0300

    Archaeological Site solution

---
+++ Exercises/JuniorM/ArchaeologicalTest2/ArchaeologicalTest2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArchaeologicalSite
+{
+    [TestClass]
+    public class ArchaeologicalSite
+    {
+        [TestMethod]
+        public void BuildingArea()
+        {
+
+            double area = CalculateArea(1, 2, 5, 2, 4, 1);
+            Assert.AreEqual(2, area);
+
+        }
+
+        double CalculateArea(double firstLongitude, double firstLatitude, double secondLongitude, double secondLatitude, double thirdLongitude, double thirdLatitude)
+        {
+            double area = (firstLongitude * secondLatitude + secondLongitude * thirdLatitude + firstLatitude * thirdLongitude - thirdLongitude * secondLatitude - firstLatitude * secondLongitude - firstLongitude * thirdLatitude) / 2;
+            return Math.Abs(area);
+
+        }
+    }
+}
