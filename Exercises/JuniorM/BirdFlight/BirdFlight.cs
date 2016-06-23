ad702ab25b5314b2f075cd7f434c7532138285e1
Flo-go <florentina.nicoara@yahoo.com>
2016-06-23 10:56:29 +0300

    Bird Flight Solution

---
+++ Exercises/JuniorM/BirdFlight/BirdFlight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BirdFlight
+{
+    [TestClass]
+    public class BirdFlight
+    {
+        [TestMethod]
+
+        public void BirdFlightSolution()
+        {
+            double flightDistance = CalculateBirdFlight(35);
+            Assert.AreEqual(26.25, flightDistance);
+        }
+
+        double CalculateBirdFlight(double distanceBetweenTrains)
+        {
+            return distanceBetweenTrains*3/4;
+        }
+
+    }
+}
