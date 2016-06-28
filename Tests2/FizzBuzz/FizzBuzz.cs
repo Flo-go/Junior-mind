13ac66b878cf632b62cb69367f9fb41aadcb5c6a
Flo-go <florentina.nicoara@yahoo.com>
2016-06-29 01:48:17 +0300

    FizzBuzz Solution

---
+++ Tests2/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzz
+    {
+        [TestMethod]
+        public void FizzBuzzTest()
+        {
+            Assert.AreEqual("FizzBuzz", DetermineFizzBuzz(60));
+        }
+        string DetermineFizzBuzz(int number)
+        {
+            int reminderFromThree;
+            int reminderFromFive;
+            Math.DivRem(number, 3, out reminderFromThree);
+            Math.DivRem(number, 5, out reminderFromFive);
+            if (reminderFromThree == 0 & reminderFromFive == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (reminderFromThree == 0)
+            {
+                return "Fizz";
+            }
+            return "Buzz";
+        }
+    }
+}
