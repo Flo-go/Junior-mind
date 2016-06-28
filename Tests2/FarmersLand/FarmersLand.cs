2f1a86e046f8b98eae19c054601b7237f0617e02
Flo-go <florentina.nicoara@yahoo.com>
2016-06-29 01:19:29 +0300

    Farmer's Field Solution

---
+++ Tests2/FarmersLand/FarmersLand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmersLand
+{
+    [TestClass]
+    public class FarmersLand
+    {
+        [TestMethod]
+        public void FarmersLandTest()
+        {
+            Assert.AreEqual(770, CalculateFieldInitialSize(230, 770000));
+        }
+        double CalculateFieldInitialSize (double additionalFieldLength, double finalFieldArea)
+        {
+            double discriminant = Math.Pow(additionalFieldLength, 2) + 4 * finalFieldArea;
+            return (Math.Sqrt(discriminant)-additionalFieldLength)/2;
+        }
+        
+    }
+}
