8e46cd42d439f1dc49ad7b61292f287afb710fdf
Flo-go <florentina.nicoara@yahoo.com>
2016-06-28 23:30:08 +0300

    Watermelon solution

---
+++ Tests2/Watermelon/WatermelonTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Watermelon
+{
+    [TestClass]
+    public class WatermelonTest
+    {
+        [TestMethod]
+        public void WatermelonSlices()
+        {
+            Assert.AreEqual("DA", EvenSlicesSolution(54));
+        }
+        string EvenSlicesSolution(int kilos)
+        {
+            int reminder;
+            int quotient = Math.DivRem(kilos, 2, out reminder);
+            string evenSlices = reminder > 0 ^ quotient==1 ? "NU" : "DA";
+            return evenSlices;
+        } 
+
+
+    }
+}
