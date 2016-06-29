2bd4109a8adea3143b00816af341a623be601925
Flo-go <florentina.nicoara@yahoo.com>
2016-06-30 00:15:59 +0300

    Calculate penalty for short, medium and long term

--- Tests2/DelayPenalty/DelayPenaltyTest.cs
+++ Tests2/DelayPenalty/DelayPenaltyTest.cs
@@ -9,19 +9,38 @@ namespace DelayPenalty
         [TestMethod]
         public void PenaltyForShortTerm()
         {
-            Assert.AreEqual(2, CalculatePenalty(100,5));
+            Assert.AreEqual(4, CalculatePenalty(200,5));
         }
 
         [TestMethod]
         public void PenaltyForMediumTerm()
         {
-            Assert.AreEqual(5, CalculatePenalty(100, 20));
-        }
-        double CalculatePenalty(int rent, int days)
-        { 
-            int percentPenalty = days < 11 ? 2 : 5;
-            return rent*percentPenalty/100;
+            Assert.AreEqual(10, CalculatePenalty(200, 20));
         }
 
+        [TestMethod]
+        public void PenaltyForLongTerm()
+        {
+            Assert.AreEqual(20, CalculatePenalty(200, 35));
+        }
+        double CalculatePenalty(int rent, int days)
+        {
+            int percentPenalty = 2;
+            if (IsMediumTerm(days))
+                percentPenalty = 5;
+            else
+             if (IsLongTerm(days))
+                    percentPenalty = 10;
+           return rent * percentPenalty / 100;
+        }
+
+        private static bool IsMediumTerm(int days)
+        {
+            return days >10 && days<31;
+        }
+        private static bool IsLongTerm(int days)
+        {
+            return days > 30 && days< 41;
+        }
     }
 }
