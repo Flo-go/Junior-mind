2b6562d44c7843ed8a88a4ac80989c3b9dedb27b
Flo-go <florentina.nicoara@yahoo.com>
2016-06-29 23:41:49 +0300

    Calculate Penalty for Short Term

---
+++ Tests2/DelayPenalty/DelayPenaltyTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DelayPenalty
+{
+    [TestClass]
+    public class DelayPenaltyTest
+    {
+        [TestMethod]
+        public void PenaltyForShortTerm()
+        {
+            Assert.AreEqual(2, CalculatePenalyForShortTerm(100));
+        }
+        double CalculatePenalyForShortTerm(int rent)
+        {
+            double penalty = rent * 2 / 100;
+            return penalty;
+        }
+
+    }
+}
