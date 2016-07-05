2a7fec4f7aefccefb837e8dcd6147bc46b972297
Flo-go <florentina.nicoara@yahoo.com>
2016-07-05 22:28:01 +0300

    Roman Numerals, Array Solution

---
+++ Tests2/RomanNumerals/RomanNumeralsTestTwo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumerals
+{
+    [TestClass]
+    public class RomanNumeralsTest
+    {
+        
+        [TestMethod]
+        public void NumeralsConversion()
+        {
+            Assert.AreEqual("LXXXVII",ConvertToRomanNumerals(87));
+        }
+
+        string ConvertToRomanNumerals(int number)
+        {
+            string [] romanNumeralsSmallerThanTen= {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+            string [] romanNumeralsGreaterThanTen = {"XX","XXX","XL","L","LX","LXX","LXXX","XC","C"};
+            int tens = number / 10;
+            int ones = number % 10;
+            switch (tens)
+            {
+                case 0:
+                    return romanNumeralsSmallerThanTen[ones - 1];
+                case 10:
+                    return romanNumeralsGreaterThanTen[tens - 2];
+                default:
+                    return romanNumeralsGreaterThanTen[tens - 2] + romanNumeralsSmallerThanTen[ones - 1];
+            }
+        }
+        
+    }
+}
